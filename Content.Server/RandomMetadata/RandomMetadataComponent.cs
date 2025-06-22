2d0b85c8bc9b23411d86791fcc51db37d5c37351
mikus <153551970+mikusssssss@users.noreply.github.com>
2025-06-22 21:04:09 +0200

    fix

--- Content.Server/RandomMetadata/RandomMetadataComponent.cs
+++ Content.Server/RandomMetadata/RandomMetadataComponent.cs
@@ -1,4 +1,8 @@
-﻿namespace Content.Server.RandomMetadata;
+
+using Content.Shared.Dataset;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.RandomMetadata;
 
 /// <summary>
 ///     Randomizes the description and/or the name for an entity by creating it from list of dataset prototypes or strings.
@@ -6,15 +10,24 @@
 [RegisterComponent]
 public sealed partial class RandomMetadataComponent : Component
 {
-    [DataField("descriptionSegments")]
-    public List<string>? DescriptionSegments;
+    [DataField]
+    public List<ProtoId<LocalizedDatasetPrototype>>? DescriptionSegments;
 
-    [DataField("nameSegments")]
-    public List<string>? NameSegments;
+    [DataField]
+    public List<ProtoId<LocalizedDatasetPrototype>>? NameSegments;
+    
+    /// Goobstation start
+    /// <summary>
+    /// LocId of the formatting string to use to assemble the <see cref="NameSegments"/> into the entity's name.
+    /// Segments will be passed to the localization system with this string as variables named $part0, $part1, $part2, etc.
+    /// </summary>
+    [DataField]
+    public LocId NameFormat = "random-metadata-name-format-default";
 
-    [DataField("nameSeparator")]
-    public string NameSeparator = " ";
-
-    [DataField("descriptionSeparator")]
-    public string DescriptionSeparator = " ";
+    /// <summary>
+    /// LocId of the formatting string to use to assemble the <see cref="DescriptionSegments"/> into the entity's description.
+    /// Segments will be passed to the localization system with this string as variables named $part0, $part1, $part2, etc.
+    /// </summary>
+    [DataField]
+    public LocId DescriptionFormat = "random-metadata-description-format-default"; /// Goobstation end
 }
